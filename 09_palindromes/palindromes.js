ae7dcfea52c11eee8dbd62bc0f473e6818fc1977
Thomas <osucollab@gmail.com>
2023-10-09 00:21:05 -0400

    adds solution checking if a string is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (palindromeCheck) {
+    let isPalindrome = true;
+    let delimiters = ", !.";
+    for (let i = 0; i < delimiters.length; i++) { palindromeCheck = palindromeCheck.replaceAll(delimiters[i], ''); }
+    //console.log(palindromeCheck);
+    palindromeCheck = palindromeCheck.toLowerCase();
+    for (let i = 0; i < palindromeCheck.length/2 && isPalindrome !== false; i++)
+    {
+        if (palindromeCheck[i] !== palindromeCheck[palindromeCheck.length-1-i])
+            isPalindrome = false;
+    }
+    return isPalindrome;
 };
 
 // Do not edit below this line
