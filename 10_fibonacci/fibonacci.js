32e1bba871c20e1cf766c883ffd164e684eac0ae
Thomas <osucollab@gmail.com>
2023-11-05 15:09:37 -0500

    added solution for JS exercise 10: fibonacci
    adds solution for the JS exercise #10 for fibonacci sequence
    given a particular value. All given test cases passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(fibonacciVal) {
+    if (fibonacciVal < 1) { return "OOPS"; }
+    let prev1 = 0;
+    let prev2 = 0;
+    let next;
 
+    for (let i = 0; i < fibonacciVal; i++)
+    {
+        next = prev1 + prev2;
+        if (next === 0) { next = 1; }
+        prev1 = prev2;
+        prev2 = next;
+    }
+    return next;
 };
 
 // Do not edit below this line
